6755ab5ec07a67511e571ef1a586502e17297fd5
E-Code30 <edwardjefta55@gmail.com>
2023-06-07 23:15:20 +0800

    Initial commit

---
+++ FastBubble.py
@@ -0,0 +1,19 @@
+def bubble_sort(arr):
+    n = len(arr)
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        for i in range(1, n):
+            if arr[i - 1] > arr[i]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+                swapped = True
+
+        n -= 1
+
+    return arr
+
+arr = [5, 2, 8, 12, 3]
+sorted_arr = bubble_sort(arr)
+print(sorted_arr)
